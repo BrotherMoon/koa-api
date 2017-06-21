fdc3134bcdd16f7c49f17c5815c866910bf2925c
xuwenchao <842390367@qq.com>
2017-06-22 00:25:50 +0800

    测试添加token认证

--- src/middlewares/response.js
+++ src/middlewares/response.js
@@ -13,10 +13,10 @@
 module.exports = async(ctx, next) => {
     ctx.error = ({data, msg, status, error}) => {
         ctx.status = status || 400;
-        ctx.body = {code: -200, msg, data, error};
+        ctx.body = {msg, data, error};
     }
     ctx.success = ({data, msg}) => {
-        ctx.body = {code: 200, msg, data};
+        ctx.body = {msg, data};
     }
     await next()
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,17 @@
 const router = require('koa-router')();
 const userCtrl = require('../controllers/user.controller');
 const blogCtrl = require('../controllers/blog.controller');
+const checkToken = require('../middlewares/checkToken');
 
 /*用户相关路由*/
-router.get('/user', userCtrl.findUsers)
-    .get('/user/:name', userCtrl.findUser)
+router.get('/user', checkToken, userCtrl.findUsers)
+    .get('/user/:name', checkToken, userCtrl.findUser)
     .post('/user', userCtrl.createUser)
     .post('/user/login', userCtrl.login)
 
 /*博客文章相关路由*/
-router.post('/blog', blogCtrl.createBlog)
-    .get('/blog', blogCtrl.findBlogs)
-    .delete('/blog', blogCtrl.deleteBolg)
+router.post('/blog', checkToken, blogCtrl.createBlog)
+    .get('/blog', checkToken, blogCtrl.findBlogs)
+    .delete('/blog', checkToken, blogCtrl.deleteBolg)
 
 module.exports = router;
