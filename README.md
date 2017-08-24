effe1a4ee02e15ae6d5b8532e10c86dc66b185b1
xuwenchao <842390367@qq.com>
2017-08-25 00:06:39 +0800

    添加htpp status说明

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# koa api
+# Koa Api
 个人项目中使用到的restful api接口
+## Api返回状态说明
+|状态码|含义|说明|
+|-----|---|---|
+|200|OK|请求成功|
+|201|CREATED|创建成功|
+|202|ACCEPTED|更新成功|
+|400|BAD REQUEST|请求的地址不存在或者包含不支持的参数|
+|401|UNAUTHORIZED|未授权|
+|403|FORBIDDEN|被禁止访问|
+|404|NOT FOUND|请求的资源不存在|
+|500|INTERNAL SERVER ERROR|内部错误|
+### 发生错误时，HTTP Status Code为4xx
+错误格式
+```
+{
+ "msg":"uri_not_found",
+ "code":1001
+}
+```
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,11 @@ const blogCtrl = require('../controllers/blog.controller')
 const checkToken = require('../middlewares/checkToken')
 /*用户相关路由*/
 router.get('/user', userCtrl.findUsers)
-    .get('/user/:name', userCtrl.findUser)
-    .post('/user', userCtrl.createUser)
-    .post('/user/login', userCtrl.login)
+      .get('/user/:name', userCtrl.findUser)
+      .post('/user', userCtrl.createUser)
+      .post('/user/login', userCtrl.login)
 /*博客文章相关路由*/
 router.post('/blog', blogCtrl.createBlog)
-    .get('/blog', blogCtrl.findBlogs)
-    .delete('/blog', blogCtrl.deleteBolg)
+      .get('/blog', blogCtrl.findBlogs)
+      .delete('/blog', blogCtrl.deleteBolg)
 module.exports = router
